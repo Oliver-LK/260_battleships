2d2679ce7d25d34d7dce9bd270dd91bc793b7ea6
Michael Rivers <mri76@uclive.ac.nz>
2022-10-11 13:29:48 +1300

    Added display

--- game.c
+++ game.c
@@ -3,27 +3,18 @@
 #include "timer.h"
 #include "navswitch.h"
 #include <stdint.h>
-
-static const pio_t rows[] =
-{
-    LEDMAT_ROW1_PIO, LEDMAT_ROW2_PIO, LEDMAT_ROW3_PIO, 
-    LEDMAT_ROW4_PIO, LEDMAT_ROW5_PIO, LEDMAT_ROW6_PIO,
-    LEDMAT_ROW7_PIO
-};
-
-static const pio_t columns[] =
-{
-    LEDMAT_COL1_PIO, LEDMAT_COL2_PIO, LEDMAT_COL3_PIO,
-    LEDMAT_COL4_PIO, LEDMAT_COL5_PIO
-};
+#include <stdbool.h>
+#include "display.h"
 
 int main (void)
 {
-    system_init ();
-    ship_setup();
+    display_init();
 
-    while (1)
-    {
+    display_pixel_set(0, 0, true);
+    display_pixel_set(0, 6, true);
+    display_pixel_set(4, 0, true);
 
+    while (1) {
+        display_update();
     }
 }
