393b7635caecf232a2fa253d89f6d3c646739ab8
Michael Rivers <mri76@uclive.ac.nz>
2022-10-18 10:14:14 +1300

    Pre branch move commit

---
+++ irfuncs.c
@@ -0,0 +1,25 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include "ir_uart.h"
+#include "system.h"
+#include "pio.h"
+
+bool player1_check(void) {}
+    bool player1 = false;
+    char recieved;
+    if (ir_uart_read_ready_p()) {
+        recieved = ir_uart_getc();
+        ir_uart_putc('!');
+    } else {
+        bool player1 = true;
+        bool waitingforopp = true;
+        while (waitingforopp) {
+            ir_uart_putc('!');
+            if (ir_uart_read_ready_p()) {
+                recieved = ir_uart_getc();
+                waitingforopp = false;
+            }
+        }
+    }
+}
---
+++ irfuncs.h
@@ -0,0 +1,9 @@
+#ifndef IRFUNCS_H
+#define IRFUNCS_H
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+bool player1_check(void);
+
+#endif
